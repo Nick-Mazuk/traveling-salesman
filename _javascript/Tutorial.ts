32253d6ce4208ee32641f10ace9d036a6b3baa2a
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-07-16 11:24:14 -0700

    Typo again

--- _javascript/Tutorial.ts
+++ _javascript/Tutorial.ts
@@ -48,14 +48,14 @@ export class Tutorial {
     ]
 
     panelImages = [
-        './assets/logo/logo.svg',
+        './assets/logo/Logo.svg',
         './assets/addHouses.gif',
         './assets/updateRealTime.gif',
         './assets/chooseAlgorithm.gif',
         './assets/algorithmComparison.gif',
         './assets/cityGrid.gif',
         './assets/more.gif',
-        './assets/logo/logo.svg',
+        './assets/logo/Logo.svg',
     ]
 
     constructor () {
